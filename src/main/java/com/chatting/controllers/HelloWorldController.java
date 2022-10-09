3fbda3b036d03d3d3baeb7a7d1cbd5a0213a7a8f
sungslee <k8440009@gmail.com>
2022-10-09 16:08:37 +0900

    add react

---
+++ src/main/java/com/chatting/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.chatting.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
