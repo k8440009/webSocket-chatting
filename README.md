131167f9c6af85a23348138bbaf4ea7a1f3f1e02
sungslee <k8440009@gmail.com>
2022-10-09 17:15:10 +0900

    add react view

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 1. 실행
 1) build 후 -> 서버 실행
 - ./gradlew build
+- java -jar build/libs/chatting-0.0.1-SNAPSHOT.jar
 - 서버 실행
 2) bootRun 
 - ./gradlew bootRun
--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,7 +2,48 @@ import React from "react";
 import axios from 'axios';
 
 function App() {
+    return (
+        <div id="main-content">
 
+            <div className="row">
+                <div className="col-md-6">
+                    <form className="form-inline">
+                        <div className="form-group">
+                            <label htmlFor="connect">WebSocket connection:</label>
+                            <button id="connect" type="submit">Connect</button>
+                            <button id="disconnect" type="submit" disabled="disabled">Disconnect</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-12">
+                    <table id="conversation" className="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>Greetings</th>
+                        </tr>
+                        </thead>
+                        <tbody id="greetings">
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6">
+                    <form className="form-inline">
+                        <div className="form-group">
+                            <input type="text" id="name" className="form-control" placeholder="Your name here..."></input>
+                        </div>
+                        <button id="send" className="btn btn-default" type="submit">Send</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 // import React, {useEffect, useState} from 'react';
